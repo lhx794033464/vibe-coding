93eca0868533b1259bb8b989160b5c2f1fad9d39
user1810034969 <221446928875044-user1810034969@noreply.coze.cn>
2026-03-22 21:39:08 +0800

    fix: 修复头像上传后无法显示的问题
    - 在next.config.ts中添加对象存储域名到images.remotePatterns
    - 添加coze-coding-project.tos.coze.site和*.tos.coze.site域名
    - 重启开发服务器应用配置

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,16 @@ const nextConfig: NextConfig = {
         hostname: 'lf-coze-web-cdn.coze.cn',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'coze-coding-project.tos.coze.site',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.tos.coze.site',
+        pathname: '/**',
+      },
     ],
   },
 };
